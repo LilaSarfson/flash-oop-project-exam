c6ba26265b9a9a092188611c5bd1386776e51e24
LilaSarfson <lilamandarina@hotmail.com>
2022-04-14 13:45:02 +0200

    exam

--- js/app.js
+++ js/app.js
@@ -13,7 +13,50 @@ Vue.createApp({
                 question: "¿Cómo hago para relacionar una variable de estado con el campo <em>value</em>' de input?",
                 answer: "La directiva v-model nos ayudará en este caso"
 
-            }]
+            }],
+            Addquestion: false,
+
+            Questions: "",
+            Answers: "",
+            id: 0,
+            // EditedNote: false,
+        }
+
+    },
+    methods: {
+        DeleteNote(note) {
+            this.notes = this.notes.filter((n) => n.id != note.id)
+
+
+        },
+        AddQuestion() {
+            this.Addquestion = true;
+        },
+
+        AddNote() {
+            if (this.notes.lenth > 1) {
+                this.id = this.notes[this.notes.length - 1].id + 1
+            }
+            else {
+                this.id++;
+            }
+            let newNote = { id: this.id, question: this.Questions, answer: this.Answers }
+            this.notes.push(newNote)
+            this.Addquestion = false;
+            this.Questions = "";
+            this.Answers = "";
+            console.log(this.notes)
+
+        },
+        EditNote(note) {
+            let editedNote = this.notes.find((n) => n.id == note.id)
+            this.EditedNote = true;
+            this.AddQuestion();
+            this.Questions = editedNote.question;
+            this.Answers = editedNote.answer;
+            this.DeleteNote(editedNote)
+
+
         }
     }
 }).mount('#app')
